a7ac6eab88d224539899f6b3ad6ecf8c87d4ebbf
Trevor Jones <tjones009@yahoo.com>
2020-03-03 16:55:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,7 +56,14 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
   
-  cart
+  cart.keys.each do |item|
+    if cart[item][:clearance]
+    cart[item][:price] = (cart[item][:price]*0.80).round(2)
+  end
+end
+cart
+end
+    
   
 end
 
