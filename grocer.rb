9e3e653c426c939fa5c452af2171e2b79a06cd92
Trevor Jones <tjones009@yahoo.com>
2020-03-03 18:23:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
   i = 0
-  while i < collection.size
+  while i < collection.length
     if collection[i][:item] == name
       collection[i]
     end
