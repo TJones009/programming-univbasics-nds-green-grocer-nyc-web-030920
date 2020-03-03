3c87422594a1a52742c6aeaea5c3e48ff5c180fe
Trevor Jones <tjones009@yahoo.com>
2020-03-03 20:22:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,13 +45,13 @@ def apply_coupons(cart, coupons)
   
   i = 0
   while counter < coupons.length
-    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
     couponed_item_name = "#{coupons[i][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-    if cart_item && cart_item[:count] >= coupons[counter][:num]
+    if cart_item && cart_item[:count] >= coupons[i][:num]
       if cart_item_with_coupon
-        cart_item_with_coupon[:count] += coupons[counter][:num]
-        cart_item[:count] -= coupons[counter][:num]
+        cart_item_with_coupon[:count] += coupons[i][:num]
+        cart_item[:count] -= coupons[i][:num]
       else
         cart_item_with_coupon = {
           :item => couponed_item_name,
@@ -60,7 +60,7 @@ def apply_coupons(cart, coupons)
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon
-        cart_item[:count] -= coupons[counter][:num]
+        cart_item[:count] -= coupons[i][:num]
       end
     end  
     i += 1
