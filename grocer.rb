a0b2a18c3864eb52bfbccfd909b27dd64bc68392
Trevor Jones <tjones009@yahoo.com>
2020-03-03 17:27:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,10 +5,11 @@ def find_item_by_name_in_collection(name, collection)
   
   i = 0
   while i < collection.size
-    collection[i]
+    if collection[i][:item] == name
+      collection[i]
+    end
     i += 1
   end
-  
 end
 
 def consolidate_cart(cart)
