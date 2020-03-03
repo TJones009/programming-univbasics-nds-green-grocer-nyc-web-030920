7bf6cca0425e30fcf594697a6b321c18ebf955c2
Trevor Jones <tjones009@yahoo.com>
2020-03-03 16:58:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,4 +77,8 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  consol_cart = consolidate_cart(cart)
+  coupons_applied = 
+  
 end
