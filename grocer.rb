716ec227e870d9aee5ec167aebd488cd8a51a486
Trevor Jones <tjones009@yahoo.com>
2020-03-03 20:11:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,7 +56,7 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => couponed_item_name,
           :price => coupons[counter][:cost] / coupons[counter][:num],
-          
+          :count => coupons[i]
         }
     end  
     i += 1
